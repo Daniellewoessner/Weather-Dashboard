2c37a30a5ade31ff8dbf7ec97077cbf9275efa19
Danielle Woessner <dlegare7792@gmail.com>
2025-01-02 20:30:22 -0500

    Delete Weather-Dashboard directory

--- Weather-Dashboard/server/src/routes/index.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-import apiRoutes from './api/index.js';
-import htmlRoutes from './htmlRoutes.ts';
-router.use('/api', apiRoutes);
-router.use('/', htmlRoutes);
-export default router;
